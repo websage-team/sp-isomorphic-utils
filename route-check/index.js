e1d69dc28d7e3a69e7a7a36170598acde7b8d55e
Diablohu <diablohudream@gmail.com>
2017-10-28 11:27:21 +0800

    [routerCheck] added check for __SERVER__ || __SPA__

--- route-check/index.js
+++ route-check/index.js
@@ -1,1 +1,5 @@
-module.exports = (nextState, isAlwaysTrue) => (isAlwaysTrue || typeof location === 'undefined') ? true : (nextState.location.pathname === location.pathname)
+module.exports = (nextState, isAlwaysTrue) => (
+    (isAlwaysTrue || typeof location === 'undefined' || __SERVER__ || __SPA__)
+        ? true
+        : (nextState.location.pathname === location.pathname)
+)
